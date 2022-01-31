7a533bce00e6bf684973268c4aa54a25d258fbb7
ConnorWang1688 <connorwang1688@gmail.com>
2022-01-31 14:35:08 -0800

    Added in cv2 stuff and some edits to the HTML

--- Website/main.py
+++ Website/main.py
@@ -1,4 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+import cv2
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+camera = cv2.VideoCapture(0)
+
+
 app = Flask(  # Create a flask app
 	__name__,
 	template_folder='templates',  # Name of html file folder
@@ -10,10 +27,15 @@ def hello_world():
   return render_template("status.html")
 
 @app.route('/', methods=['POST'])
-def hellow_world_post():
+def hello_world_post():
   first_name = request.form['fname']
   last_name = request.form['lname']
-  return render_template("home.html", message = "You are a loser, " + first_name + " " + last_name)
+  return render_template("status.html", message = "You are a loser, " + first_name + " " + last_name)
 
-  
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 app.run(host='0.0.0.0', port=8080)
+
